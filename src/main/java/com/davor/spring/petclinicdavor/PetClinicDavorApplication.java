087e0a30a905f4c05cd1338235280fb62f95485b
Jorge Lara <larajorge11@gmail.com>
2021-10-19 20:42:51 -0500

    feat(First commit)

---
+++ src/main/java/com/davor/spring/petclinicdavor/PetClinicDavorApplication.java
@@ -0,0 +1,13 @@
+package com.davor.spring.petclinicdavor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicDavorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicDavorApplication.class, args);
+	}
+
+}
