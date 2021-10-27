854d9b307069b199262911212cc368bbf8985751
Jorge Lara <larajorge11@gmail.com>
2021-10-26 19:45:44 -0500

    feat(Create html index pages and controllers for vets, owner and index)

--- pet-clinic-web/src/main/java/PetClinicDavorApplication.java
+++ pet-clinic-web/src/main/java/PetClinicDavorApplication.java
@@ -1,9 +1,9 @@
-package petclinicdavor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.davor.spring"})
 public class PetClinicDavorApplication {
 
 	public static void main(String[] args) {
